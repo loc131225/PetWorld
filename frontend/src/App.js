ecedd12bbb540d52a98d2d4590df98f99a40167f
khanhdang1905 <khanhdang190504@gmail.com>
2025-06-15 21:35:40 +0700

    trang thanh toán

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import ThanhToan from './pages/Thanhtoan';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <Routes>
+    <Route path=''>
+      <Route path='thanhtoan' element={<ThanhToan/>}/>
+    </Route>
+  </Routes>
+  </>;
 }
 
 export default App;
