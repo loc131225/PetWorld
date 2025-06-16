eaa71e5dafd92b4fc786945476da5759f176d92e
loc131225 <mloctx131225@gmail.com>
2025-06-16 12:59:15 +0700

    Add files via upload

---
+++ backend/app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::whereNull('parent_id')
+            ->where('status', 1)
+            ->with(['children' => function ($query) {
+                $query->where('status', 1)->take(6);
+            }])
+            ->get()
+            ->map(function ($parent) {
+                return [
+                    'id' => $parent->id,
+                    'name' => $parent->name,
+                    'image' => $parent->image,
+                    'children' => $parent->children->map(function ($child) {
+                        return [
+                            'id' => $child->id,
+                            'name' => $child->name,
+                            'image' => $child->image,
+                        ];
+                    }),
+                ];
+            });
+
+        return response()->json($categories);
+    }
+}
