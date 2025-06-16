cfa5e9a1402471c5f2911648fcacf0d5c0eb51b2
loc131225 <mloctx131225@gmail.com>
2025-06-16 16:10:33 +0700

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,29 @@
 <?php
+
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use Illuminate\Http\Request;
 
-Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::get('/categoriesTree', [CategoryController::class, 'index']);
+
+// Người dùng
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Bảo vệ các route cần xác thực
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+
+    Route::post('/logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Đăng xuất thành công',
+        ]);
+    });
+});
