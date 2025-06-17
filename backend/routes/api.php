3b781749b65cfbbca8299b66b60556382bba6ec8
BeeThowj <hungnps40061@gmail.com>
2025-06-17 14:52:40 +0700

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Admin\ProductController;
+use App\Http\Controllers\Api\Admin\CategoryController;
 
 
 Route::get('/categoriesTree', [CategoryController::class, 'index']);
@@ -26,4 +28,21 @@ Route::middleware('auth:sanctum')->group(function(){
             'message' => 'Đăng xuất thành công',
         ]);
     });
+});
+
+//Admin
+Route::prefix('admin/products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/', [ProductController::class, 'store']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::post('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::prefix('admin/categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::post('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
 });
