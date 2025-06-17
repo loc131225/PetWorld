66f99edb3b3fce11f81fb76c30049c0d56431930
BeeThowj <hungnps40061@gmail.com>
2025-06-17 14:50:16 +0700

    Update Category.php

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,6 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $table = 'categories';
+    protected $fillable = ['name', 'image', 'description', 'status'];
+
+    public function categoryProduct_category()
+    {
+        return $this->hasMany(Product_category::class);
+    }
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_categories', 'category_id', 'product_id');
+    }
+    
     public function children(){
         return $this->hasMany(Category::class, 'parent_id');
     }
