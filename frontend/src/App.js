3bd1fdfade50b68a3fc91b4c0193c517a94b1bed
khanhdang1905 <khanhdang190504@gmail.com>
2025-06-17 21:39:38 +0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import ThanhToan from './pages/Thanhtoan';
+import LoginRegister from './pages/Loginregister';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return <>
   <Routes>
     <Route path=''>
       <Route path='thanhtoan' element={<ThanhToan/>}/>
+      <Route path='home' element={<Home/>}/>
+      <Route path='cart' element={<Cart/>}/>
     </Route>
+    <Route path='login' element={<LoginRegister/>}/>
   </Routes>
   </>;
 }
