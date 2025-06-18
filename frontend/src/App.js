745b0c2521ab4cab32de49dbe8cb5b24436e1fdd
tkien536 <nt.kienn0701@gmail.com>
2025-06-18 21:42:59 +0700

    import
    up app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import ThanhToan from './pages/Thanhtoan';
-import LoginRegister from './pages/Loginregister';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
+import ThanhToan from './pages/users/Thanhtoan';
+import LoginRegister from './pages/users/Loginregister';
+import Home from './pages/users/Home';
+import Cart from './pages/users/Cart';
+import CategoryList from './pages/admin/CategoryList';
+import ProductTable from './pages/admin/ProductTable';
+import UserList from './pages/admin/UserList';
+import ProductDetail from './pages/users/ProductDetail';
+import Timkiem from './pages/users/Timkiem';
 
 function App() {
   return <>
@@ -13,8 +18,12 @@ function App() {
       <Route path='thanhtoan' element={<ThanhToan/>}/>
       <Route path='home' element={<Home/>}/>
       <Route path='cart' element={<Cart/>}/>
+      <Route path='CategoryList' element={<CategoryList/>}/>
+      <Route path='ProductTable' element={<ProductTable/>}/>
+      <Route path='UserList' element={<UserList/>}/>
+      <Route path='ProductDetail' element={<ProductDetail/>}/>
+      <Route path='Timkiem' element={<Timkiem/>}/>
     </Route>
-    <Route path='login' element={<LoginRegister/>}/>
   </Routes>
   </>;
 }
