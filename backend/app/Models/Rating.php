d987367d470c9b505ba177cb59090d452dfddc37
loc131225 <mloctx131225@gmail.com>
2025-06-23 11:54:17 +0700

    Update Rating.php

--- backend/app/Models/Rating.php
+++ backend/app/Models/Rating.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     protected $table = 'ratings';
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
