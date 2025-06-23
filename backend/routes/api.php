ac07c90bef14bfaf3b0a5222d54f39c6241c86d6
loc131225 <mloctx131225@gmail.com>
2025-06-23 11:26:49 +0700

    Update api.php
    thêm api và đổi tên controller cho admin để tránh lỗi trùng lặp

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,12 +3,26 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\VoucherController;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Api\Admin\ProductController;
-use App\Http\Controllers\Api\Admin\CategoryController;
+use App\Http\Controllers\Api\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Api\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Api\Admin\UserController as AdminUserController;
 
 
 Route::get('/categoriesTree', [CategoryController::class, 'index']);
+Route::get('/products/home-dogs', [ProductController::class, 'dogProducts']);
+Route::get('/products/home-cats', [ProductController::class, 'catProducts']);
+Route::get('/products/home-best-sellers', [ProductController::class, 'bestSellingProducts']);
+Route::get('/home-news', [PostController::class, 'homeNews']);
+Route::get('/vouchers/lastest', [VoucherController::class, 'lastestVouchers']);
+Route::get('/vouchers/petworldvoucher', [VoucherController::class, 'getPetworldVoucher']);
+
+//Chi tiết sản phẩm
+Route::get('/products/{slug}', [ProductController::class, 'show']);
 
 // Người dùng
 Route::post('/register', [AuthController::class, 'register']);
@@ -30,26 +44,29 @@ Route::middleware('auth:sanctum')->group(function(){
     });
 });
 
+// Thanh toán
+Route::middleware('auth:sanctum')->post('/checkout', [OrderController::class, 'checkout']);
+
 //Admin
 Route::prefix('admin/products')->group(function () {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::post('/', [ProductController::class, 'store']);
-    Route::get('/{id}', [ProductController::class, 'show']);
-    Route::post('/{id}', [ProductController::class, 'update']);
-    Route::delete('/{id}', [ProductController::class, 'destroy']);
+    Route::get('/', [AdminProductController::class, 'index']);
+    Route::post('/', [AdminProductController::class, 'store']);
+    Route::get('/{id}', [AdminProductController::class, 'show']);
+    Route::post('/{id}', [AdminProductController::class, 'update']);
+    Route::delete('/{id}', [AdminProductController::class, 'destroy']);
 });
 
 Route::prefix('admin/categories')->group(function () {
-    Route::get('/', [CategoryController::class, 'index']);
-    Route::post('/', [CategoryController::class, 'store']);
-    Route::get('/{id}', [CategoryController::class, 'show']);
-    Route::post('/{id}', [CategoryController::class, 'update']);
-    Route::delete('/{id}', [CategoryController::class, 'destroy']);
+    Route::get('/', [AdminCategoryController::class, 'index']);
+    Route::post('/', [AdminCategoryController::class, 'store']);
+    Route::get('/{id}', [AdminCategoryController::class, 'show']);
+    Route::post('/{id}', [AdminCategoryController::class, 'update']);
+    Route::delete('/{id}', [AdminCategoryController::class, 'destroy']);
 });
 
 Route::prefix('admin/users')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/{id}/toggle-status', [UserController::class, 'toggleStatus']);
-    Route::post('/{id}/toggle-role', [UserController::class, 'toggleRole']);
+    Route::get('/', [AdminUserController::class, 'index']);
+    Route::post('/{id}/toggle-status', [AdminUserController::class, 'toggleStatus']);
+    Route::post('/{id}/toggle-role', [AdminUserController::class, 'toggleRole']);
 });
 
