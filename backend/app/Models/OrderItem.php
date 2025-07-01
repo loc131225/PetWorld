dc720b829e9dce0d6e3cf7bdb54713ab402b66a9
BeeThowj <hungnps40061@gmail.com>
2025-07-01 10:40:37 +0700

    Update OrderItem.php

--- backend/app/Models/OrderItem.php
+++ backend/app/Models/OrderItem.php
@@ -6,12 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
+    protected $table = 'order_items';
+
     protected $fillable = [
         'order_id',
         'product_attribute_id',
-        'product_name',
-        'price',
         'quantity',
-        'total',
+        'price',
+        'total'
     ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id');
+    }
+
+    public function productAttribute()
+    {
+        return $this->belongsTo(ProductAttribute::class, 'product_attribute_id');
+    }
+
+    // Shortcut để truy xuất sản phẩm qua product_attribute
+    public function product()
+    {
+        return $this->hasOneThrough(
+            Product::class,
+            ProductAttribute::class,
+            'id', // FK ProductAttribute
+            'id', // FK Product
+            'product_attribute_id', // Local
+            'product_id' // Intermediate
+        );
+    }
 }
