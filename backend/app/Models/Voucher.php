a1b2466a1f0717f5d06570fb557dbd70b62cef6a
BeeThowj <hungnps40061@gmail.com>
2025-07-01 10:41:10 +0700

    Update Voucher.php

--- backend/app/Models/Voucher.php
+++ backend/app/Models/Voucher.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Voucher extends Model
 {
-    //
+    protected $table = 'vouchers';
+
+    protected $fillable = [
+        'name',
+        'code',
+        'discount_type',
+        'discount_value',
+        'min_order_value',
+        'start_date',
+        'end_date',
+        'quantity',
+        'status'
+    ];
+
+    protected $dates = [
+        'start_date',
+        'end_date',
+        'deleted_at'
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'voucher_id');
+    }
 }
