8f72c44e114d4b11e653848ee4abf8ba537252f4
BeeThowj <hungnps40061@gmail.com>
2025-07-01 10:40:16 +0700

    Update Order.php

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -6,16 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $table = 'orders'; // Tên bảng
+
     protected $fillable = [
         'user_id',
         'payment_id',
         'voucher_id',
         'status',
-        'shipping_address',
         'phone',
-        'name',
-        'total_amount',
+        'shipping_address',
         'note',
+        'total_amount',
         'payment_status',
+        'payment_date'
     ];
+
+    protected $casts = [
+        'payment_date' => 'datetime'
+    ];
+
+    // Khách hàng
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // Voucher nếu có
+    public function voucher()
+    {
+        return $this->belongsTo(Voucher::class, 'voucher_id');
+    }
+
+    // Phương thức thanh toán
+    public function paymentMethod()
+    {
+        return $this->belongsTo(PaymentMethod::class, 'payment_id');
+    }
+
+    // Danh sách sản phẩm trong đơn
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id');
+    }
 }
