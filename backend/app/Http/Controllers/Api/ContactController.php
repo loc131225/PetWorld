071c3eb101418edd00f48e332bfb1f9418d8fc06
BeeThowj <hungnps40061@gmail.com>
2025-07-02 10:46:43 +0700

    Add files via upload

---
+++ backend/app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\ContactReplyMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function showForm()
+    {
+        return view('contact');
+    }
+
+    public function submit(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+        ]);
+
+        $data = (object)[
+            'name' => $request->name,
+            'email' => $request->email,
+        ];
+
+        Mail::to($request->email)->send(new ContactReplyMail($data));
+
+        return response()->json(['message' => 'Thông tin đã được gửi. Vui lòng kiểm tra email của bạn.']);
+    }
+}
+
+
