c1500790f3e88e046e804f1da5c3df338349b827
loc131225 <mloctx131225@gmail.com>
2025-07-04 14:16:59 +0700

    Update FavoriteProduct.php

--- backend/app/Models/FavoriteProduct.php
+++ backend/app/Models/FavoriteProduct.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class FavoriteProduct extends Model
 {
     protected $table = 'favorite_products';
+
+    protected $fillable = ['user_id', 'product_id'];
+
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
