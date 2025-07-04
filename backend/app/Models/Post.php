7e084b3ce6cc9621fb15741f3efda0e43c8c647f
HauBlackk <trunghauvoo3105@gmail.com>
2025-07-04 23:03:29 +0700

    Update Post.php

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -1,10 +1,20 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'post_cate_id', 'title', 'content', 'thumbnail', 'status'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(PostCategory::class, 'post_cate_id');
+    }
 }
+
